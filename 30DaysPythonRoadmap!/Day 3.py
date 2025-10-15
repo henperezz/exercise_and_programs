b1ea953840978ee30995c51ae697ffe0e529eec3
Pedro Henrique Perez <pedro.hen.perez08@gmail.com>
2025-10-15 13:42:02 -0300

    Add files via upload

---
+++ 30DaysPythonRoadmap!/Day 3.py
@@ -0,0 +1,45 @@
+# Day 3 - 30 days python roadmap (operadores)
+
+# Inteiros
+
+print('Adição, 1+1=', 1+1)  # 3
+print('Subtração, 2-1=', 2-1)  # 1
+print('Multiplicação, 3*3=', 3*3)  # 9
+print('Divisão, 4/2=', 4/2)  # 2
+print('Exponenciação, 2**2=', 2**2)  # 4
+print('Resto da divisão, 4/3=', 4 % 3, '4/2=', 4 % 2)  # 1, 0
+print('Divisão inteira. 4/3=', 4//3)  # 1
+print('\n')
+
+# Flutuantes
+
+print('Flutuantes PI', 3.14)
+print('Flutuantes Gravidade', 9.81)
+print('\n')
+# Complexos
+
+print('Complexos', 1 + 1j)
+print('Multiplicação de complexos,', (1 + 1j) * (5 + 17j))
+print('\n')
+
+#  Fazer as operações com variáveis com números atribuídos
+
+a = 10
+b = 2
+
+total = a + b  # 12
+diferenca = a - b  # 8
+multiplicacao = a * b  # 20
+divisao = a / b  # 5
+exponenciacao = a ** b  # 100
+resto_da_divisao = a % b  # 0
+divisao_total = a // b  # 5
+
+print(f'a + b = {total}')
+print(f'a - b = {diferenca}')
+print(f'a * b = {multiplicacao}')
+print(f'a / b = {divisao}')
+print(f'a ** b = {exponenciacao}')
+print(f'a % b = {resto_da_divisao}')
+print(f'a // b = {divisao_total}')
+
